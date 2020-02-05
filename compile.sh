c25388632ce577f85a890bedd9f34e603ee2165f
lawrence910426 <lawrence910426@gmail.com>
2020-02-05 23:43:58 +0800

    Update compile.sh
    Former-commit-id: 731a298982aa41f010299978464b6080bbaebbf5

--- compile.sh
+++ compile.sh
@@ -1,6 +1,9 @@
 # This script will compile the Release version of Factory Client and publish it into server_constant_files
+rm -r Factory-Frontend
 
 git clone https://github.com/dinnersystem/Factory-Frontend
 zip client.zip Factory-Frontend/FactoryClient/FactoryClient/bin/Release/
 
 mv client.zip server_constant_files
+
+rm -r Factory-Frontend
