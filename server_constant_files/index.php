1633ea09bb07205038b9a94e728ad7553b1e81d8
lawrence910426 <lawrence910426@gmail.com>
2020-02-05 23:22:33 +0800

    Update index.php
    Former-commit-id: 0c750372a5aa445742bdda523d12a3c42c9070de

--- server_constant_files/index.php
+++ server_constant_files/index.php
@@ -1,15 +1,3 @@
 <?php
-    # this file requires a bom header to let ios plugin run
-    ini_set('display_errors' ,1);
-    error_reporting(0);
-    # mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
-    
-    require_once(__DIR__ . "/backend_proc/backend_main.php");
-    date_default_timezone_set('Asia/Taipei');
-
-    $backend_main = new backend_proc\backend_main();
-    $result = $backend_main->run();
-
-    if(is_string($result)) echo $result;
-    else echo \json\json_output::output($result);
+	 header("Location: /dinnersys_beta/frontend");
 ?>
