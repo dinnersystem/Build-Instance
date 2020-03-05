3d6248e6a2a469c0a665c3ebccffa97f8a32fd64
lawrence910426 <lawrence910426@gmail.com>
2020-03-05 11:46:41 +0800

    Click Once Publish

--- update.sh
+++ update.sh
@@ -8,11 +8,14 @@ mkdir /usr/share/nginx/html/dinnersys_beta
 mkdir /usr/share/nginx/html/dinnersys_beta/frontend
 mkdir /usr/share/nginx/html/dinnersys_beta/backend
 mkdir /usr/share/nginx/html/dinnersys_beta/factory_client
+mkdir /usr/share/nginx/html/dinnersys_beta/fclient
 
 git clone https://github.com/dinnersystem/Frontend-Website
 git clone https://github.com/dinnersystem/Backend
+git clone https://github.com/dinnersystem/Factory-Frontend
 cp -r Frontend-Website/* /usr/share/nginx/html/dinnersys_beta/frontend
 cp -r Backend/* /usr/share/nginx/html/dinnersys_beta/backend
+cp -r Factory-Frontend/FactoryClient/FactoryClient/publish/* /usr/share/nginx/html/dinnersys_beta/fclient 
 
 cp -r server_constant_files/u_move_u_dead /usr/share/nginx/html/dinnersys_beta/frontend/u_move_u_dead
 cp server_constant_files/config.php /usr/share/nginx/html/dinnersys_beta/backend/backend_proc/config.php
