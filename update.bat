a33b7a9b12383bfb94951ae1a8b41ffd8ba873b0
lawrence910426 <lawrence910426@gmail.com>
2020-04-27 12:25:00 +0800

    Update update.bat

--- update.bat
+++ update.bat
@@ -1,4 +1,4 @@
 rmdir /s /q C:\wamp64\www\dinnersys_beta\backend
 rmdir /s /q C:\wamp64\www\dinnersys_beta\frontend
-robocopy D:\github\Backend C:\wamp64\www\dinnersys_beta\backend /e /xd .git
-robocopy D:\github\Frontend-Website C:\wamp64\www\dinnersys_beta\frontend /e /xd .git
+robocopy D:\github\dinnersystem\Backend C:\wamp64\www\dinnersys_beta\backend /e /xd .git
+robocopy D:\github\dinnersystem\Frontend-Website C:\wamp64\www\dinnersys_beta\frontend /e /xd .git
